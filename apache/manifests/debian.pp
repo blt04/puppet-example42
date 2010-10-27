141657e1818cb04ca67f3fdb5a2adecba0c78781
Brandon Turner <bturner@bltweb.net>
2010-10-27 16:51:37 -0500

    [apache] Don't create the logs link until Apache is installed

--- apache/manifests/debian.pp
+++ apache/manifests/debian.pp
@@ -8,6 +8,8 @@ class apache::debian {
 # Quick'n'dirt fix for logs directory in apache::virtualhost base template
     file { "Apache_loglink":
         path   => "/etc/apache2/logs",
-        ensure => "/var/log/apache2/",
+        ensure => "link",
+        target => "/var/log/apache2/",
+        require => Package['apache'];
     }
 }
